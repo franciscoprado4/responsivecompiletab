b077229f20ced67342c01433d5b04a685188c402
francjpdev <javier4prado@gmail.com>
2024-09-19 16:50:11 -0600

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Navbar from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
 import { FeatureSection } from "./components/FeatureSection";
-import { Workflow } from "lucide-react";
+import { Workflow } from "./components/Workflow";
+import { Pricing } from "./components/Pricing";
+import { Testimonials } from "./components/Testimonials";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <FeatureSection />
-      <Workflow />
+      <main className="container mx-auto">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </main>
     </>
   );
 };
--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -2,7 +2,7 @@ import { features } from "../constants";
 
 export const FeatureSection = () => {
   return (
-    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+    <div className="relative mt-20 mx-8 border-b border-neutral-800 min-h-[800px]">
       <div className="text-center">
         <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
           feature
@@ -16,7 +16,7 @@ export const FeatureSection = () => {
       </div>
       <div className="flex flex-wrap mt-10 lg:mt-20">
         {features.map((feature, index) => (
-          <div className="w-full sm:w-1/2 lg:w-1/3">
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
             <div className="flex">
               <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
                 {feature.icon}
